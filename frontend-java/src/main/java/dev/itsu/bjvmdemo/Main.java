8a29b1250e1b65cf2104c38f13b0dd42e789c05e
itsu-dev <itsu020402@gmail.com>
2025-02-19 08:47:58 +0900

    feat: 要素の取得を実装

--- frontend-java/src/main/java/dev/itsu/bjvmdemo/Main.java
+++ frontend-java/src/main/java/dev/itsu/bjvmdemo/Main.java
@@ -1,7 +1,17 @@
 package dev.itsu.bjvmdemo;
 
+import dev.itsu.bjvm.$;
+import org.w3c.dom.html.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Hello from Main.java!");
+
+        var posts = (HTMLDivElement) $.document.getElementById("posts");
+        var newPost = (HTMLTextAreaElement) $.document.getElementById("newPost");
+        var name = (HTMLInputElement) $.document.getElementById("name");
+        var textCount = (HTMLParagraphElement) $.document.getElementById("textCount");
+        var postButton = (HTMLButtonElement) $.document.getElementById("postButton");
+
     }
 }
