3a85ab20a03e3b63afd34f99747baf16e82f7a53
itsu-dev <itsu020402@gmail.com>
2025-02-19 13:50:30 +0900

    feat: フォームが動くように実装

--- frontend-java/src/main/java/dev/itsu/bjvmdemo/Main.java
+++ frontend-java/src/main/java/dev/itsu/bjvmdemo/Main.java
@@ -1,17 +1,45 @@
 package dev.itsu.bjvmdemo;
 
-import dev.itsu.bjvm.$;
-import org.w3c.dom.html.*;
+import dev.itsu.bjvm.*;
+import dev.itsu.dom.html.*;
+import org.w3c.dom.events.Event;
+import org.w3c.dom.events.EventListener;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Main.java!");
 
         var posts = (HTMLDivElement) $.document.getElementById("posts");
-        var newPost = (HTMLTextAreaElement) $.document.getElementById("newPost");
+        var newPost = (HTMLTextareaElement) $.document.getElementById("newPost");
         var name = (HTMLInputElement) $.document.getElementById("name");
-        var textCount = (HTMLParagraphElement) $.document.getElementById("textCount");
+        var textCount = (HTMLPElement) $.document.getElementById("textCount");
         var postButton = (HTMLButtonElement) $.document.getElementById("postButton");
 
+        newPost.addEventListener("input", new EventListener() {
+            @Override
+            public void handleEvent(Event evt) {
+                var length = String.valueOf(newPost.getValue().length());
+                textCount.setTextContent(length);
+            }
+        });
+
+        postButton.addEventListener("click", new EventListener() {
+            @Override
+            public void handleEvent(Event evt) {
+                var length = newPost.getValue().length();
+                if (length == 0) {
+                    $.alert("Post cannot be empty!");
+                    return;
+                }
+
+                var nameLength = name.getValue().length();
+                if (nameLength == 0) {
+                    $.alert("Name cannot be empty!");
+                    return;
+                }
+
+                System.out.println(name.getValue() + ": " + newPost.getValue());
+            }
+        });
     }
 }
