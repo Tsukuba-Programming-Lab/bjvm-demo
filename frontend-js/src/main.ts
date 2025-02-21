1e56f9664f18a40e86f307e23bcdf63ae1c3351e
itsu-dev <itsu020402@gmail.com>
2025-02-21 12:51:37 +0900

    fix: ビルドエラーを修正

--- frontend-js/src/main.ts
+++ frontend-js/src/main.ts
@@ -2,21 +2,23 @@ import launchBJVM from 'bjvm';
 import './style.css';
 
 try {
-    const stdOut = document.getElementById("stdOut")!;
-    const options = {
-        stdErrHandler: (text: string) => {
-            stdOut.innerHTML += `<span style="color: red;">${text}</span>`;
-        },
-        stdOutHandler: (text: string) => {
-            stdOut.innerHTML += `<span>${text}</span>`;
-        },
-        externalLibraries: [
-            await fetchAsUint8Array("java/lib/dom.zip"),
-            await fetchAsUint8Array("java/lib/bjvm-webapi-bindings.jar"),
-        ],
-    };
+    (async () => {
+        const stdOut = document.getElementById("stdOut")!;
+        const options = {
+            stdErrHandler: (text: string) => {
+                stdOut.innerHTML += `<span style="color: red;">${text}</span>`;
+            },
+            stdOutHandler: (text: string) => {
+                stdOut.innerHTML += `<span>${text}</span>`;
+            },
+            externalLibraries: [
+                await fetchAsUint8Array("java/lib/dom.zip"),
+                await fetchAsUint8Array("java/lib/bjvm-webapi-bindings.jar"),
+            ],
+        };
 
-    await launchBJVM(await fetchAsUint8Array("java/app.jar"), options);
+        await launchBJVM(await fetchAsUint8Array("java/app.jar"), options);
+    })();
 } catch (e) {
     console.log(e);
 }
